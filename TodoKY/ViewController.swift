6ffd1cd7b951d541232a79fc908fdf38b67d18c4
artyamamoto <yamamoto.k@art-bank.co.jp>
2014-09-10 15:10:15 +0900

    Initial Commit

---
+++ TodoKY/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodoKY
+//
+//  Created by 山本和明 on 2014/09/10.
+//  Copyright (c) 2014年 Kazuaki Yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
